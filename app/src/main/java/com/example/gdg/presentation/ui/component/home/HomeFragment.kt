39cea2141e2166774fecc10c37f91d0070abd3f9
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-03 03:02:07 +0900

    홈 부분 중간 퍼블리싱

---
+++ app/src/main/java/com/example/gdg/presentation/ui/component/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.gdg.presentation.ui.component.home
+
+import com.example.gdg.R
+import com.example.gdg.databinding.FragmentHomeBinding
+import com.example.gdg.ui.base.BaseFragment
+
+class HomeFragment: BaseFragment<FragmentHomeBinding> (R.layout.fragment_home) {
+    override fun init() {
+
+    }
+}
