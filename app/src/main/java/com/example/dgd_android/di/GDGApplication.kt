f75bb30f286a0379e794cf833cef0014c190db89
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-03 01:09:13 +0900

    di 세팅

---
+++ app/src/main/java/com/example/dgd_android/di/GDGApplication.kt
@@ -0,0 +1,7 @@
+package com.example.dgd_android.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GDGApplication : Application()
---
+++ app/src/main/java/com/example/dgd_android/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.example.dgd_android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+}
