a4340d84d4f6523d486f9f457833e1e690f10741
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-12-03 01:09:25 +0900

    baseActivity, baseFragment 적용

---
+++ app/src/main/java/com/example/dgd_android/presentation/base/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dgd_android.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layout: Int) :
+    AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layout)
+        binding.lifecycleOwner = this
+        init()
+    }
+
+    abstract fun init()
+}
---
+++ app/src/main/java/com/example/dgd_android/presentation/component/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dgd_android.presentation.component.main
+
+import com.example.dgd_android.presentation.base.BaseActivity
+import com.example.scpandroid.R
+import com.example.scpandroid.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+
+}
